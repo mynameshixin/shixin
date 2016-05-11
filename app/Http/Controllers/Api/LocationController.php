74ceac3d85249d8b4d71195e0bb6846047a1a6b2
shixin <shixin19890130@163.com>
2016-05-11 15:58:45 +0800

    空文件夹bug

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -24,14 +24,14 @@ class LocationController extends BaseController{
      *       summary="获取地理位置",
      *       @SWG\Parameters(
      *         @SWG\Parameter(
-     *           name="uid",
+     *           name="user_id",
      *           description="用户id",
      *           paramType="query",
      *           required=true,
      *           type="string"
      *         ),
      *         @SWG\Parameter(
-     *           name="token",
+     *           name="access_token",
      *           description="token",
      *           paramType="query",
      *           required=true,
@@ -46,14 +46,18 @@ class LocationController extends BaseController{
     public function postIndex(){
     	$data = Input::all();
     	$data = fparam($data);
-        $rules = array (
-            'uid' =>'required|exists:users,id',
-            'token' =>'required|min:5|max:50',
-        );
+        $rules = [
+            'user_id' =>'required|exists:users,id',
+            'access_token' =>'required|min:5|max:50',
+        ];
+        $message = [
+        	'user_id.required'=>'用户uid不存在',
+        	'access_token.required'=>'令牌token 5-50位',
+        ];
         //请求参数验证
-        parent::validator($data, $rules);
-        parent::validateAcessToken($data['token']);
-        $user = DB::table('users')->select('id','location','location_x','location_y')->where('id',$data['uid'])->first();
+        parent::validator($data, $rules,$message);
+        parent::validateAcessToken($data['access_token']);
+        $user = DB::table('users')->select('id','location','location_x','location_y')->where('id',$data['user_id'])->first();
         if(!empty($user)) return response(['data'=>$user,'code'=>200,'message'=>'请求成功']);
         return response(['code'=>1001,'message'=>'请求失败']);
         
@@ -69,14 +73,14 @@ class LocationController extends BaseController{
      *       summary="获取地理位置",
      *       @SWG\Parameters(
      *         @SWG\Parameter(
-     *           name="uid",
+     *           name="user_id",
      *           description="用户id",
      *           paramType="form",
      *           required=true,
      *           type="string"
      *         ),
      *         @SWG\Parameter(
-     *           name="token",
+     *           name="access_token",
      *           description="token",
      *           paramType="form",
      *           required=true,
@@ -112,22 +116,29 @@ class LocationController extends BaseController{
     public function postAfferent(){
     	$data = Input::all();
     	$data = fparam($data);
-        $rules = array (
-            'uid' =>'required|exists:users,id',
-            'token' =>'required|min:5|max:50',
+        $rules = [
+            'user_id' =>'required|exists:users,id',
+            'access_token' =>'required|min:5|max:50',
             'location' =>'required|max:30',
             'location_x' =>'required',
             'location_y' =>'required',
-        );
+        ];
+        $message = [
+        	'user_id.required'=>'用户uid不存在',
+        	'access_token.required'=>'令牌token 5-50位',
+        	'location.required'=>'位置不能为空',
+        	'location_x.required'=>'位置经度不能为空',
+        	'location_y.required'=>'位置纬度不能为空',
+        ];
         //请求参数验证
-        parent::validator($data, $rules);
-        parent::validateAcessToken($data['token']);
+        parent::validator($data, $rules,$message);
+        parent::validateAcessToken($data['access_token']);
         $updateData  = [
         	'location'=>$data['location'],
         	'location_x'=>$data['location_x'],
         	'location_y'=>$data['location_y']
         ];
-        $user = DB::table('users')->where('id',$data['uid'])->update($updateData);
+        $user = DB::table('users')->where('id',$data['user_id'])->update($updateData);
         if($user) return response(['data'=>$user,'code'=>200,'message'=>'更新定位成功']);
         return response(['code'=>1001,'message'=>'更新定位失败']);
     }
