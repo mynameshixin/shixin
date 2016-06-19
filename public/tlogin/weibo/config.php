850f0135c2cec33cb5fb9ac82fe643fb51a97129
shixin <shixin19890130@163.com>
2016-06-19 18:37:52 +0800

    7.4 42

---
+++ public/tlogin/weibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '1833470990' );
+define( "WB_SKEY" , '7aaf89ec0e2e8c565724c26bffa57d11' );
+define( "WB_CALLBACK_URL" , 'http://www.duitujia.com/webd/tlogin/weiboback' );
