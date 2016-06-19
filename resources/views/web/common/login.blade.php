7f7e92f36ead8462ecfd8714327f6bafdbd90c1c
shixin <shixin19890130@163.com>
2016-06-19 23:35:39 +0800

    7.5 7

--- resources/views/web/common/login.blade.php
+++ resources/views/web/common/login.blade.php
@@ -51,8 +51,9 @@
         <div class="pop_login_conother">
           <a href="/webd/tlogin/weibo?rurl=<?php echo urlencode($_SERVER['REQUEST_URI']);?>" target="_blank"></a>
           <a href="/webd/tlogin/wechat?rurl=<?php echo urlencode($_SERVER['REQUEST_URI']);?>" target="_blank"></a>
-          <a href="/webd/tlogin/qq?rurl=<?php echo urlencode($_SERVER['REQUEST_URI']);?>" target="_blank"></a>
-          <a href="javascript:;" style="margin-right: 0px;"></a>
+          <!-- <a href="/webd/tlogin/qq?rurl=<?php echo urlencode($_SERVER['REQUEST_URI']);?>" target="_blank"></a> -->
+          <a href="javascript:;" onclick="alert('功能测试中')" target="_blank"></a>
+          <a href="javascript:;" onclick="alert('功能测试中')" style="margin-right: 0px;"></a>
         </div>
         <div class="pop_login_contit">
           使用手机号登录
