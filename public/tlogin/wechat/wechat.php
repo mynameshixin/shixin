e30f0059001f76f0d83939c860c03ae15e06cc3d
shixin <shixin19890130@163.com>
2016-06-19 16:05:02 +0800

    7.4 24

--- public/tlogin/wechat/wechat.php
+++ public/tlogin/wechat/wechat.php
@@ -56,6 +56,7 @@ class Wechat
   public function gettoken($code = ''){
     $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->appid}&secret={$this->appsecret}&code={$code}&grant_type=authorization_code";
     $res =  https_request($url);
+    return $res;
     return json_decode($res,1);
   }
 }
