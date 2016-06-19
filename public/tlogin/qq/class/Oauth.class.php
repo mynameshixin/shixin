1afa72ef1d5d45e5beabced946db6462ab398b81
shixin <shixin19890130@163.com>
2016-06-20 02:05:54 +0800

    7.5 9

--- public/tlogin/qq/class/Oauth.class.php
+++ public/tlogin/qq/class/Oauth.class.php
@@ -54,9 +54,9 @@ class Oauth{
         $state = $this->recorder->read("state");
 
         //--------验证state防止CSRF攻击
-        if($_GET['state'] != $state){
+       /* if($_GET['state'] != $state){
             $this->error->showError("30001");
-        }
+        }*/
 
         //-------请求参数列表
         $keysArr = array(
