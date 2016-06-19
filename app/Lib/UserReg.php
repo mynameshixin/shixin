681a6e764204b9dc18ffda1d1ef1409f7aff25f5
shixin <shixin19890130@163.com>
2016-06-20 06:13:50 +0800

    l2

--- app/Lib/UserReg.php
+++ app/Lib/UserReg.php
@@ -305,7 +305,7 @@ class UserReg
             $data['wechat'] = isset($params['wechat']) ? $params['wechat'] : '';
             $data['auth_avatar'] = isset($params['headimgurl']) ? $params['headimgurl'] : '';
             if(isset($params['gender']))$data['gender'] = $params['sex'];
-            dd($data);
+            var_dump($data);die;
             User::where('wechat_id',$id)->update($data);
         }
         $row = $row->toArray();
