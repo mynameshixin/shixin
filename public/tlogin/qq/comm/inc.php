4ab9ea0ac4484274cfdaec610a4585d805c17926
shixin <shixin19890130@163.com>
2016-06-20 03:17:58 +0800

    7.5 13

--- public/tlogin/qq/comm/inc.php
+++ public/tlogin/qq/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"1104987288","appkey":"1dnPuuXwgmeUP88Y","callback":"http://www.duitujia.com/webd/qq/callback","scope":"get_user_info","errorReport":true,"storageType":"file","host":"www.duitujia.com","user":"root","password":"root","database":"test"}
+{"appid":"1104987288","appkey":"1dnPuuXwgmeUP88Y","callback":"http://www.duitujia.com/qq/callback","scope":"get_user_info","errorReport":true,"storageType":"file","host":"www.duitujia.com","user":"root","password":"root","database":"test"}
