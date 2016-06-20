69782616290a65a959a899650ab8b77163275b88
shixin <shixin19890130@163.com>
2016-06-21 07:10:11 +0800

    7.8 11

--- app/Http/Controllers/Web/PicsController.php
+++ app/Http/Controllers/Web/PicsController.php
@@ -78,9 +78,9 @@ class PicsController extends CmController{
 		$self_id = $this->user_id;
 		$folder = DB::table('goods')->where('id',$id)->select('folder_id')->first();
 		if(!$folder) die('no such pic!');
-		/*$private = DB::table('folders')->where('id',$folder['folder_id'])->select('private','user_id')->first();
+		$private = DB::table('folders')->where('id',$folder['folder_id'])->select('private','user_id')->first();
 		$cg = DB::table('collection_good')->where(['user_id'=>$self_id,'good_id'=>$id])->first();
-		if($private['private']==1 && $self_id!=$private['user_id'] && !$cg) die('such pic is in a private folder!');*/
+		if($private['private']==1 && $self_id!=$private['user_id'] && !$cg) die('such pic is in a private folder!');
 		$goods = ProductWebsupply::get_pic_detail($self_id,$data);
 		// dd($goods);
 		$data = [
