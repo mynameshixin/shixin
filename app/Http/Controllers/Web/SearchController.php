9f0a674d4aab538b56e4b19fa1b6a2bd9931f8b5
shixin <shixin19890130@163.com>
2016-06-21 06:58:49 +0800

    7.8 10

--- app/Http/Controllers/Web/SearchController.php
+++ app/Http/Controllers/Web/SearchController.php
@@ -98,17 +98,28 @@ class SearchController extends CmController{
         $num = isset($data['num']) ? $data['num'] : 15;
         $page = isset($data['page'])?$data['page']:1;
     	$skip = ($page-1)*$num;
-        $goods = DB::table('goods as g')->join('folder_goods as fg','g.id','=','fg.good_id')->select('g.id', 'g.user_id', 'g.folder_id', 'g.kind', 'g.price', 'g.reserve_price', 'g.image_ids', 'g.title', 'g.tags',  'g.description', 'g.collection_count', 'g.praise_count', 'g.boo_count', 'g.detail_url', 'g.created_at')->where(
-        	function($query) use ($keyword){
-        		$query->where('g.title', "like", "%{$keyword}%")->orWhere('g.tags', "like", "%{$keyword}%");
-        	})->orderBy('g.created_at','desc');
-        if(isset($data['kind'])) $goods = $goods->where('g.kind','=',$data['kind']);
         if(isset($data['user_id'])){
-        	$user_id = self::get_user_cache($data['user_id']);
-        	if(!DB::table('users')->where('id',$user_id)->first()) return response()->forApi([],1001,'用户不存在');
-        	$goods = $goods->where('fg.user_id',$user_id);
+            $user_id = self::get_user_cache($data['user_id']);
+            if(!DB::table('users')->where('id',$user_id)->first()) return response()->forApi([],1001,'用户不存在');
+            $goods = DB::table('goods as g')->join('folder_goods as fg','g.id','=','fg.good_id')->select('g.id', 'g.user_id', 'g.folder_id', 'g.kind', 'g.price', 'g.reserve_price', 'g.image_ids', 'g.title', 'g.tags',  'g.description', 'g.collection_count', 'g.praise_count', 'g.boo_count', 'g.detail_url', 'g.created_at')->where(
+            function($query) use ($keyword){
+                $query->where('g.title', "like", "%{$keyword}%")->orWhere('g.tags', "like", "%{$keyword}%");
+            })->orderBy('g.created_at','desc');
+            $goods = $goods->where('fg.user_id',$user_id);
+            if(isset($data['kind'])) $goods = $goods->where('g.kind','=',$data['kind']);
+            $goods = $goods->skip($skip)->take($num)->get();
+        }else{
+            $goods = DB::table('goods as g')->select('g.id', 'g.user_id', 'g.folder_id', 'g.kind', 'g.price', 'g.reserve_price', 'g.image_ids', 'g.title', 'g.tags',  'g.description', 'g.collection_count', 'g.praise_count', 'g.boo_count', 'g.detail_url', 'g.created_at')->where(
+            function($query) use ($keyword){
+                $query->where('g.title', "like", "%{$keyword}%")->orWhere('g.tags', "like", "%{$keyword}%");
+            })->orderBy('g.created_at','desc');
+        
+            if(isset($data['kind'])) $goods = $goods->where('g.kind','=',$data['kind']);
+            $goods = $goods->skip($skip)->take($num)->get();
         }
-        $goods = $goods->skip($skip)->take($num)->get();
+        
+
+        
         foreach ($goods as $key => $value) {
         	$cuser = DB::table('collection_good')->where('good_id',$value['id'])->select('user_id','folder_id')->orderBy('created_at','desc')->first();
         	$cfolder = DB::table('folders')->where('id',$cuser['folder_id'])->select('name','id')->first();
