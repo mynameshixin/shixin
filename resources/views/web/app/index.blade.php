60d85817856f951e03a30cbe5b11429c006d3a11
shixin <shixin19890130@163.com>
2016-06-22 01:17:04 +0800

    7.12 18

--- resources/views/web/app/index.blade.php
+++ resources/views/web/app/index.blade.php
@@ -4,24 +4,32 @@
 <script type="text/javascript" src="{{asset('web')}}/js/nolog.js"></script>
 <body class="nolog_body">
 	@include('web.common.banner')
-	<div class="container nolog_index_container clearfix">
-		<div class="nolog_index_conapp">
+
+
+		<div class="nolog_index_container">
+		<div class="nolog_index_conapp clearfix" style="background:none">
 			<div class="w1248 clearfix" style="padding-top: 1px;">
 				<div class="nolog_index_conapp_phone">
-					<img src="{{asset('web')}}/images/10.png" alt="">
+					<img src="{{asset('web')}}/images/cat/10.png" alt="">
 				</div>
 				<div class="nolog_index_conapp_right">
 					<img src="{{asset('web')}}/images/app_logo.png" height="146" width="146" class="nolog_app_log"></img>
-					<p class="nolog_app_font">我们一起寻找好东西</p>
+					<p class="nolog_app_font">用社交的方式链接全球家居资源</p>
 					<div class="nolog_app_code clearfix">
 						<div class="nolog_app_wrap">
-							<img src="{{asset('web')}}/images/code_an.png" height="146" width="146" alt="">
-							<p>Android 下载</p>
+							<img src="{{asset('web')}}/images/code_an.png" height="128" width="128" alt="">
+							<p style="color:#828282">Android 下载</p>
 						</div>
 						<div class="nolog_app_wrap">
-							<img src="{{asset('web')}}/images/code_ip.png" height="146" width="146" alt="">
-							<p>iPhone/iPad 下载</p>
+							<img src="{{asset('web')}}/images/code_ip.png" height="128" width="128" alt="">
+							<p style="color:#828282">iPhone/iPad 下载</p>
 						</div>
+						<!-- htmlv?=20160707 -->
+						<div class="nolog_app_wrap">
+							<img src="{{asset('web')}}/images/code_ip.png" height="128" width="128" alt="">
+							<p style="color:#828282">微信订阅号</p>
+						</div>
+						<!-- htmlv?=20160707 -->
 					</div>
 					<div class="nolog_app_qqwc">
 						<a href="#" style="margin-right: 21px;"><img src="{{asset('web')}}/images/app_wc.png" height="61" width="61" alt=""></a>
@@ -30,6 +38,7 @@
 				</div>
 			</div>
 		</div>
+	</div>
 		@include('web.common.foot')
 		
 	</div>
