10ed4af951299bf1ad51b31a2b6f5c8539aefec7
shixin <shixin19890130@163.com>
2016-06-22 08:29:37 +0800

    7.13 3

--- app/Http/Controllers/Web/PicsController.php
+++ app/Http/Controllers/Web/PicsController.php
@@ -130,7 +130,7 @@ class PicsController extends CmController{
         $user = DB::table('users')->where('id',$userId)->first();
 		if(empty($user)) return response()->forApi([],1001,'不存在的用户');
 
-		$cg = DB::table('collection_good as cg')->join('folders as f','cg.folder_id','=','f.id')->where('cg.user_id',$userId)->orderBy('cg.created_at','desc')->select('f.name','f.id','f.image_id','f.private')->take(3)->get();
+		$cg = DB::table('collection_good as cg')->join('folders as f','cg.folder_id','=','f.id')->where('cg.user_id',$userId)->orderBy('cg.created_at','desc')->groupBy('cg.folder_id')->select('f.name','f.id','f.image_id','f.private')->take(3)->get();
 		if(!empty($cg)){
 			foreach ($cg as $k => $v) {
 				$cg[$k]['image_url'] = !empty(LibUtil::getPicUrl($v['image_id'], 1))?LibUtil::getPicUrl($v['image_id'], 1):url('uploads/sundry/blogo.jpg');
