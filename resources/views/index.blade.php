4de9c5b07a0f3b07acef0b6f296f479657112d36
shixin <shixin19890130@163.com>
2016-06-22 09:17:30 +0800

    7.13 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -264,140 +264,163 @@ var _hmt = _hmt || [];
         <div class="nolog_index_catcon clearfix">
           <div class="nolog_index_catcon_left clearfix">
             <div class="nolog_index_catcon_ltit">商品分类</div>
-            <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
-              <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
-              </ul>
+            <div class="nolog_index_catcon_leftmin">
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">家具</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=沙发" target="_blank">沙发</a></li>
+                  <li><a href="/webd/search?keyword=椅子" target="_blank">椅子</a></li>
+                  <li><a href="/webd/search?keyword=书柜" target="_blank">书柜</a></li>
+                  <li><a href="/webd/search?keyword=茶几" target="_blank">茶几</a></li>
+                  <li><a href="/webd/search?keyword=床" target="_blank">床</a></li>
+                  <li><a href="/webd/search?keyword=餐桌" target="_blank">餐桌</a></li>
+                  <li><a href="/webd/search?keyword=书桌" target="_blank">书桌</a></li>
+                  <li><a href="/webd/search?keyword=衣柜" target="_blank">衣柜</a></li>
+                  <li><a href="/webd/search?keyword=电视柜" target="_blank">电视柜</a></li>
+                  <li><a href="/webd/search?keyword=鞋柜" target="_blank">鞋柜</a></li>
+                  <li><a href="/webd/search?keyword=户外家具" target="_blank">户外家具</a></li>
+                  <li><a href="/webd/search?keyword=儿童家具" target="_blank">儿童家具</a></li>
+                </ul>
+              </div>
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">装饰摆设</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=摆件" target="_blank">摆件</a></li>
+                  <li><a href="/webd/search?keyword=镜子" target="_blank">镜子</a></li>
+                  <li><a href="/webd/search?keyword=钟" target="_blank">钟</a></li>
+                  <li><a href="/webd/search?keyword=装置画" target="_blank">装置画</a></li>
+                  <li><a href="/webd/search?keyword=香薰" target="_blank">香薰</a></li>
+                  <li><a href="/webd/search?keyword=挂钩" target="_blank">挂钩</a></li>
+                  <li><a href="/webd/search?keyword=收纳" target="_blank">收纳</a></li>
+                  <li><a href="/webd/search?keyword=餐具" target="_blank">餐具</a></li>
+                  <li><a href="/webd/search?keyword=厨房用品" target="_blank">厨房用品</a></li>
+                </ul>
+              </div>
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">灯饰</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=台灯" target="_blank">台灯</a></li>
+                  <li><a href="/webd/search?keyword=吊灯" target="_blank">吊灯</a></li>
+                  <li><a href="/webd/search?keyword=壁灯" target="_blank">壁灯</a></li>
+                  <li><a href="/webd/search?keyword=户外灯" target="_blank">户外灯</a></li>
+                  <li><a href="/webd/search?keyword=镜前灯" target="_blank">镜前灯</a></li>
+                </ul>
+              </div> 
             </div>
-            <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
-              <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
-              </ul>
+            <div class="nolog_index_catcon_leftmin">
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">家纺家饰</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=床品" target="_blank">床品</a></li>
+                  <li><a href="/webd/search?keyword=抱枕" target="_blank">抱枕</a></li>
+                  <li><a href="/webd/search?keyword=布料" target="_blank">布料</a></li>
+                  <li><a href="/webd/search?keyword=窗帘" target="_blank">窗帘</a></li>
+                  <li><a href="/webd/search?keyword=坐垫" target="_blank">坐垫</a></li>
+                  <li><a href="/webd/search?keyword=桌布" target="_blank">桌布</a></li>
+                  <li><a href="/webd/search?keyword=枕头" target="_blank">枕头</a></li>
+                </ul>
+              </div>
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">卫生间</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=浴帘" target="_blank">浴帘</a></li>
+                  <li><a href="/webd/search?keyword=衣架" target="_blank">衣架</a></li>
+                  <li><a href="/webd/search?keyword=洗漱套装" target="_blank">洗漱套装</a></li>
+                  <li><a href="/webd/search?keyword=杯子" target="_blank">杯子</a></li>
+                  <li><a href="/webd/search?keyword=马桶垫" target="_blank">马桶垫</a></li>
+                  <li><a href="/webd/search?keyword=防滑垫" target="_blank">防滑垫</a></li>
+                </ul>
+              </div>
+              
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">地毯</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=现代" target="_blank">现代</a></li>
+                  <li><a href="/webd/search?keyword=古典" target="_blank">古典</a></li>
+                  <li><a href="/webd/search?keyword=手工" target="_blank">手工</a></li>
+                  <li><a href="/webd/search?keyword=动物" target="_blank">动物</a></li>
+                </ul>
+              </div>
             </div>
-            <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
-              <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
-              </ul>
+            <div class="nolog_index_catcon_leftmin">
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">花艺植物</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=多肉植物" target="_blank">多肉植物</a></li>
+                  <li><a href="/webd/search?keyword=花瓶" target="_blank">花瓶</a></li>
+                  <li><a href="/webd/search?keyword=花盆" target="_blank">花盆</a></li>
+                  <li><a href="/webd/search?keyword=仿真花" target="_blank">仿真花</a></li>
+                  <li><a href="/webd/search?keyword=鲜花" target="_blank">鲜花</a></li>
+                </ul>
+              </div>
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">品牌</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=MUJI" target="_blank">MUJI</a></li>
+                  <li><a href="/webd/search?keyword=宜家" target="_blank">宜家</a></li>
+                  <li><a href="/webd/search?keyword=zakka" target="_blank">zakka</a></li>
+                  <li><a href="/webd/search?keyword=baker" target="_blank">baker</a></li>
+                </ul>
+              </div>
+              <div class="nolog_index_cateach">
+                <div class="nolog_index_cateachtit">家用小电器</div>
+                <ul>
+                  <li><a href="/webd/search?keyword=微波炉" target="_blank">微波炉</a></li>
+                  <li><a href="/webd/search?keyword=烤箱" target="_blank">烤箱</a></li>
+                  <li><a href="/webd/search?keyword=面包机" target="_blank">面包机</a></li>
+                  <li><a href="/webd/search?keyword=咖啡机" target="_blank">咖啡机</a></li>
+                  <li><a href="/webd/search?keyword=搅拌机" target="_blank">搅拌机</a></li>
+                  <li><a href="/webd/search?keyword=其他" target="_blank">其他</a></li>
+                </ul>
+              </div>
             </div>
-            <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
-              <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
-              </ul>
-            </div>
-            <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
-              <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
-              </ul>
-            </div>
+
           </div>
           <div class="nolog_index_catcon_right">
             <div class="nolog_index_catcon_ltit">图集分类</div>
             <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
+              <div class="nolog_index_cateachtit">设计风格</div>
               <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
+                <li><a href="/webd/search?keyword=现代" target="_blank">现代</a></li>
+                <li><a href="/webd/search?keyword=中式" target="_blank">中式</a></li>
+                <li><a href="/webd/search?keyword=日式" target="_blank">日式</a></li>
+                <li><a href="/webd/search?keyword=新古典" target="_blank">新古典</a></li>
+                <li><a href="/webd/search?keyword=美式" target="_blank">美式</a></li>
+                <li><a href="/webd/search?keyword=法式" target="_blank">法式</a></li>
+                <li><a href="/webd/search?keyword=田园" target="_blank">田园</a></li>
+                <li><a href="/webd/search?keyword=地中海" target="_blank">地中海</a></li>
+                <li><a href="/webd/search?keyword=LOFT" target="_blank">LOFT</a></li>
+                <li><a href="/webd/search?keyword=北欧" target="_blank">北欧</a></li>
+                <li><a href="/webd/search?keyword=混搭" target="_blank">混搭</a></li>
               </ul>
             </div>
             <div class="nolog_index_cateach">
-              <div class="nolog_index_cateachtit">家具</div>
+              <div class="nolog_index_cateachtit">空间</div>
               <ul>
-                <li><a href="javascript:;">沙发</a></li>
-                <li><a href="javascript:;">椅子</a></li>
-                <li><a href="javascript:;">书柜</a></li>
-                <li><a href="javascript:;">茶几</a></li>
-                <li><a href="javascript:;">床</a></li>
-                <li><a href="javascript:;">餐桌</a></li>
-                <li><a href="javascript:;">书桌</a></li>
-                <li><a href="javascript:;">衣柜</a></li>
-                <li><a href="javascript:;">电视柜</a></li>
-                <li><a href="javascript:;">鞋柜</a></li>
-                <li><a href="javascript:;">户外家具</a></li>
-                <li><a href="javascript:;">儿童家具</a></li>
+                <li><a href="/webd/search?keyword=客厅" target="_blank">客厅</a></li>
+                <li><a href="/webd/search?keyword=玄关" target="_blank">玄关</a></li>
+                <li><a href="/webd/search?keyword=餐厅" target="_blank">餐厅</a></li>
+                <li><a href="/webd/search?keyword=卧室" target="_blank">卧室</a></li>
+                <li><a href="/webd/search?keyword=衣帽间" target="_blank">衣帽间</a></li>
+                <li><a href="/webd/search?keyword=书房" target="_blank">书房</a></li>
+                <li><a href="/webd/search?keyword=儿童房" target="_blank">儿童房</a></li>
+                <li><a href="/webd/search?keyword=阳台" target="_blank">阳台</a></li>
+                <li><a href="/webd/search?keyword=卫生间" target="_blank">卫生间</a></li>
+                <li><a href="/webd/search?keyword=阁楼" target="_blank">阁楼</a></li>
+                <li><a href="/webd/search?keyword=庭院" target="_blank">庭院</a></li>
               </ul>
             </div>
             <div class="nolog_index_cateach nolog_index_cateachcolor">
               <div class="nolog_index_cateachtit">颜色</div>
               <ul>
-                <li><a href="javascript:;" class="nolog_index_cr"></a></li>
-                <li><a href="javascript:;" class="nolog_index_co"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cy"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cg"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cq"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cl"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cp"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cb"></a></li>
-                <li><a href="javascript:;" class="nolog_index_cw"></a></li>
+                <li><a href="/webd/search?keyword=红色" class="nolog_index_cr" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=橙色" class="nolog_index_co" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=黄色" class="nolog_index_cy" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=绿色" class="nolog_index_cg" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=青色" class="nolog_index_cq" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=蓝色" class="nolog_index_cl" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=紫色" class="nolog_index_cp" target="_blank"></a></li>
+                <li><a href="/webd/search?keyword=黑色" class="nolog_index_cb" target="_blank"></a></li>
               </ul>
             </div>
           </div>
