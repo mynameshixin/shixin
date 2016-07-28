62aadd9457bd70fd2c398acc83580fd5ef674491
shixin <shixin19890130@163.com>
2016-07-28 17:56:03 +0800

    8.1

--- resources/views/web/common/banner/my.blade.php
+++ resources/views/web/common/banner/my.blade.php
@@ -9,9 +9,9 @@
 				<a href="#"><img src="{{!empty($self_info['auth_avatar'])?$self_info['auth_avatar']:$self_info['pic_m']}}" alt=""></a>
 				<var class="header_tril"></var>
 				<div class="header_moreinfo">
-					<a href="{{url('webd/user')}}" target="_blank" class="header_more_item header_more_1">我的堆图家</a>
+					<a href="/webd/user?oid={{$self_id}}" target="_blank" class="header_more_item header_more_1">我的堆图家</a>
 					<!-- <a href="javascript:;" target="_blank" class="header_more_item header_more_2">私信</a> -->
-					<a href="{{url('webd/user/follow')}}" target="_blank" class="header_more_item header_more_3">我的关注</a>
+					<a href="/webd/user/follow?oid={{$self_id}}" target="_blank" class="header_more_item header_more_3">我的关注</a>
 					<!-- <a href="javascript:;" target="_blank" class="header_more_item header_more_4">堆图家认证</a> -->
 					<a href="{{url('webd/user/setaccount')}}" target="_blank" class="header_more_item header_more_5">账号设置</a>
 					<a href="{{url('webd/home/logout')}}" class="header_more_item header_more_6">退出</a>
