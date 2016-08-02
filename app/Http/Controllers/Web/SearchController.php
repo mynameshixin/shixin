04378da0e506ef171f81493e4b4238d238d0fb6f
shixin <shixin19890130@163.com>
2016-08-02 17:37:35 +0800

    8.2 8

--- app/Http/Controllers/Web/SearchController.php
+++ app/Http/Controllers/Web/SearchController.php
@@ -170,65 +170,6 @@ class SearchController extends CmController{
         return response()->forApi($list);
 	}
 
-    public function getNeeds(){
-
-        $data = Input::all();
-        $data = fparam($data);
-        $rules = array(
-            'kind' => 'in:1,2',
-            'keyword'=>'required'
-        );
-        // dd($data);
-
-        //请求参数验证
-        parent::validator($data, $rules);
-        $keyword = $data['keyword'];
-        $num = isset($data['num']) ? $data['num'] : 15;
-        $page = isset($data['page'])?$data['page']:1;
-        $skip = ($page-1)*$num;
-        if(isset($data['user_id'])){
-            $user_id = self::get_user_cache($data['user_id']);
-            if(!DB::table('users')->where('id',$user_id)->first()) return response()->forApi([],1001,'用户不存在');
-            $goods = DB::table('goods as g')->join('folder_goods as fg','g.id','=','fg.good_id')->select('g.id', 'g.user_id', 'g.folder_id', 'g.kind', 'g.price', 'g.reserve_price', 'g.image_ids', 'g.title', 'g.tags',  'g.description', 'g.collection_count', 'g.praise_count', 'g.boo_count', 'g.detail_url', 'g.created_at')->where(
-            function($query) use ($keyword){
-                $query->where('g.title', "like", "%{$keyword}%")->orWhere('g.tags', "like", "%{$keyword}%");
-            })->orderBy('g.created_at','desc');
-            $goods = $goods->where('fg.user_id',$user_id);
-            if(isset($data['kind'])) $goods = $goods->where('g.kind','=',$data['kind']);
-            $goods = $goods->skip($skip)->take($num)->get();
-        }else{
-            $goods = DB::table('goods as g')->leftJoin('role_user as ru','g.user_id','=','ru.user_id')->select('g.id', 'g.user_id', 'g.folder_id', 'g.kind', 'g.price', 'g.reserve_price', 'g.image_ids', 'g.title', 'g.tags',  'g.description', 'g.collection_count', 'g.praise_count', 'g.boo_count', 'g.detail_url', 'g.created_at','ru.role_id')->where(
-            function($query) use ($keyword){
-                $query->where('g.title', "like", "%{$keyword}%")->orWhere('g.tags', "like", "%{$keyword}%");
-            })->orderBy('ru.role_id','desc')->orderBy('g.created_at','desc');
-        
-            if(isset($data['kind'])) $goods = $goods->where('g.kind','=',$data['kind']);
-            $goods = $goods->skip($skip)->take($num)->get();
-        }
-        
-
-        
-        foreach ($goods as $key => $value) {
-            $cuser = DB::table('collection_good')->where('good_id',$value['id'])->select('user_id','folder_id')->orderBy('created_at','desc')->first();
-            if(empty($cuser)){
-                $cuser = DB::table('goods')->where('id',$value['id'])->select('user_id','folder_id')->first();
-            }
-            $cfolder = DB::table('folders')->where('id',$cuser['folder_id'])->select('name','id')->first();
-            $goods[$key]['cfolder'] = !empty($cfolder)?$cfolder:[];
-            $goods[$key]['cuser'] = UserWebsupply::user_info($cuser['user_id']);
-            $goods[$key]['comment'] = CommentWebsupply::getCommentFirst($value['id']);
-            if(strpos($value['image_ids'],',') == 0){
-                $goods[$key]['image_url'] = !empty(LibUtil::getPicUrl($value['image_ids'], 1))?LibUtil::getPicUrl($value['image_ids'], 1):url('uploads/sundry/blogo.jpg');
-            }else{
-                $goods[$key]['image_url'] = url('uploads/sundry/blogo.jpg');
-            }
-        }
-        // dd($goods);
-        $list = [];
-        $list['list'] = $goods;
-        return response()->forApi($list);
-    }
-
 	// 获得文件夹
 	public function postFolder(){
         $data = fparam(Input::all());
