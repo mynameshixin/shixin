ff60964fc6679722b829a639539f8d61f24523df
shixin <shixin19890130@163.com>
2016-10-20 23:52:22 +0800

    10.21 8

--- resources/views/cq/index.blade.php
+++ resources/views/cq/index.blade.php
@@ -1,3 +1,21 @@
+<form action="/api/cq/pub" method="post" enctype="multipart/form-data">
+	
+	<input type="text" name="access_token" value="2c153eccf0d73b1a1be54283c8a30b3b"></input><br>
+	<input type="text" name="title" value="名称"></input><br>
+	<input type="text" name="description" value="描述"></input><br>
+	<input type="text" name="cityid" value="0"></input><br>
+	<input type="text" name="price" value="300"></input><br>
+	<input type="text" name="reserve_price" value="100"></input><br>
+	<input type="text" name="source" value="1"></input><br>
+	<input type="text" name="contact" value="1355698895"></input><br>
+	<input type="text" name="tags" value="分类1；分类2"></input><br>
+	<input type="file" name="image[]"></input><br>
+	<input type="file" name="image[]"></input><br>
+	<input type="submit" value="upload"></input><br>
+</form>
+<p>发布</p>
+
+
 <form action="/api/cq/edit" method="post" enctype="multipart/form-data">
 	
 	<input type="text" name="access_token" value="2c153eccf0d73b1a1be54283c8a30b3b"></input><br>
@@ -14,3 +32,5 @@
 	<input type="file" name="image[]"></input><br>
 	<input type="submit" value="upload"></input><br>
 </form>
+
+<p>修改</p>
