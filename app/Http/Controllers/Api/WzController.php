1355e3b18f4b4a1f7efde01d9b46a7b38fc950a5
root <root@ubuntu.(none)>
2016-11-25 05:37:47 +0800

    文章详情新接口

--- app/Http/Controllers/Api/WzController.php
+++ app/Http/Controllers/Api/WzController.php
@@ -117,8 +117,8 @@ class WzController extends BaseController{
         	];
         	parent::validator($data, $rules,$renews);
         $rs=WzService::getInstance()->wenzhangxiangqing($data['eassat_id']);
-        $rs['eassat_cont']='<style>img {max-width: 100%; min-width:100%;}</style>'. $rs['eassat_cont'];
-        $rs['eassat_cont']='1';
+        //$rs['eassat_cont']='<style>img {max-width: 100%; min-width:100%;}</style>'. $rs['eassat_cont'];
+        $rs['eassat_cont']='<style>img {max-width: 100%; min-width:100%;}</style><p><img src=\"http://www.duitujia.com/uploads/ueditor/image/20161122/1479804056859010.png\" title=\"1479804056859010.png\" alt=\"22.png\"/></p>';
         return response()->forApi($rs);
 	}
 	public function getComment(){
