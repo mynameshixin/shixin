bec739837ea72c70e18b3ba7a14eb6d37b2eb84c
root <root@ubuntu.(none)>
2016-12-03 02:01:21 +0800

    wz文章

--- app/Http/Controllers/Api/WzController.php
+++ app/Http/Controllers/Api/WzController.php
@@ -54,7 +54,7 @@ class WzController extends BaseController{
         		//$rs[$i]['eassat_cont']='<style>img {max-width: 100%; min-width:100%;}</style>'.$rs[$i]['eassat_cont'];
         	} 
         	   if(!$rs){
-        	   	$rs='false';
+        	   	$rs[]=null;
         	   }   	
 		 return response()->forApi($rs);
 	}
